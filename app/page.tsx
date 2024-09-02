d2b725e75f8dcf7c0d8bd5b539038a8f8a738c97
Bhavya Jaiswal <bhavyajaiswal2004@gmail.com>
2024-09-02 15:16:15 +0530

    Upload pdf

--- app/page.tsx
+++ app/page.tsx
@@ -30,9 +30,14 @@ if (e.target.files) {
   file = e.target.files[0];
 }
 
+// Determine the content type dynamically based on the file's MIME type
+const contentType = file?.type || 'application/octet-stream';  
+
 const { data, error } = await createClient().storage
   .from("testimages")
-  .upload("public/" + file?.name, file as File);
+  .upload("public/" + file?.name, file as File, {
+    contentType: contentType,
+  });
 
 if (data) {
   console.log(data);
@@ -44,7 +49,8 @@ if (data) {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <input
         type="file"
-        accept="image/*"
+        accept="*/*" //to accept all file types
+        multiple  //to upload multiple files
         className="block w-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
         id="file_input"
         onChange={(e: ChangeEvent<HTMLInputElement>) => {
